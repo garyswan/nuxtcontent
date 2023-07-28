2e8e43a005e13f2b2aa5d6562c89254fef7dee33
Gary Swanepoel <gary@pixelpixel.com.au>
2023-07-28 20:13:33 +1000

    removing 404 file

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -8,7 +8,7 @@
             NuxtLink.p-4.rounded.border(to="/",prefetchedClass="is-fetched") Home
             NuxtLink.p-4.rounded.border(to="/business",prefetchedClass="is-fetched") Business
             NuxtLink.p-4.rounded.border(to="/business/10-million-expansion-shows-confidence-in-sunshine-coast",prefetchedClass="is-fetched") Business - Real Page
-            NuxtLink.p-4.rounded.border(to="/business/10-million-expansion-shows-confidence-in-sunshine-coast-NOT",noPrefetch,prefetchedClass="is-fetched") Business - NOT Real Page
+            //- NuxtLink.p-4.rounded.border(to="/business/10-million-expansion-shows-confidence-in-sunshine-coast-NOT",noPrefetch,prefetchedClass="is-fetched") Business - NOT Real Page
             NuxtLink.p-4.rounded.border(to="/council",prefetchedClass="is-fetched") Council
 
     main.bg-red-200(class="min-h-[32rem]")
