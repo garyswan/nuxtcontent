4c32f23dbf008748f4cb1744116c349aef41eed2
Gary Swanepoel <gary@pixelpixel.com.au>
2023-07-28 12:00:22 +1000

    category only

---
+++ src/pages/[...path].vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+h1.text-4xl.font-bold.my-4 category;{{ $route.params}}
+</template>
--- src/pages/[category].vue
+++ src/pages/[category].vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const { surround, navigation } = useContent();
+const { surround, page, navigation } = useContent();
 //- navigation
 const navItems = computed(() => {
   return navigation.value;
@@ -11,33 +11,37 @@ const resetError = (error: any) => {
 };
 </script>
 <template lang="pug">
-pre navigation;{{ navigation }}
+//- pre navigation;{{ navItems }}
 .debug.flex.bg-blue-50.my-8
     pre.label default.vue
     template(v-for="item in navItems")
         div.p-4.border.rounded.h-12.flex.items-center
             //- pre item;{{ item }}
-            NuxtLink(:to="item._path") {{ item.title }}
-pre.label [category].vue
-.debug.mt-8.flex.space-x-6
-    div.w-64.bg-blue-400.prose.shrink-0.p-4
-        ul.list-disc
-            li insert files
-        template(v-for="item in surround")
-            li
-                a(:href="item._path") {{ item.title }}
-    div.bg-blue-300.grow.flex.flex-col.space-y-8
-        //- h1.text-xl page.title;{{ page.title }}
-        h1.text-xl page.title;{{ page?.title || 'nothing here' }}
-        //- pre page;{{ page }}
-        pre.label .src/[category]/[..slug].vue
-        pre below:NuxtErrorBoundary > NuxtPage    
-        div.prose
-            NuxtErrorBoundary
-                NuxtPage
-                template(#error="{ error}") 
-                    p slug page broke
-                    code {{ error }}
-                    button(@click="resetError(error)",class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded") reset error
+            NuxtLink(:to="item._path", :prefetch="false") {{ item.title }}
+
+.debug
+    pre page;{{ page }}
+
+//- pre.label [category].vue
+//- .debug.mt-8.flex.space-x-6
+//-     div.w-64.bg-blue-400.prose.shrink-0.p-4
+//-         ul.list-disc
+//-             li insert files
+//-         template(v-for="item in surround")
+//-             li
+//-                 a(:href="item._path") {{ item.title }}
+//-     div.bg-blue-300.grow.flex.flex-col.space-y-8
+//-         //- h1.text-xl page.title;{{ page.title }}
+//-         h1.text-xl page.title;{{ page?.title || 'nothing here' }}
+//-         //- pre page;{{ page }}
+//-         pre.label .src/[category]/[..slug].vue
+//-         pre below:NuxtErrorBoundary > NuxtPage    
+//-         div.prose
+//-             NuxtErrorBoundary
+//-                 NuxtPage
+//-                 template(#error="{ error}") 
+//-                     p slug page broke
+//-                     code {{ error }}
+//-                     button(@click="resetError(error)",class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded") reset error
 
 </template>
