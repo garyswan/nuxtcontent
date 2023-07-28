e8fd15276df63d88c1dc1158938e7dad03702058
Gary Swanepoel <gary@pixelpixel.com.au>
2023-07-28 14:10:22 +1000

    moving to content components

--- src/pages/[category].vue
+++ src/pages/[category].vue
@@ -14,10 +14,15 @@ const resetError = (error: any) => {
 //- pre navigation;{{ navItems }}
 .debug.flex.bg-blue-50.my-8
     pre.label default.vue
-    template(v-for="item in navItems")
-        div.p-4.border.rounded.h-12.flex.items-center
-            //- pre item;{{ item }}
-            NuxtLink(:to="item._path", :prefetch="false") {{ item.title }}
+    //- template(v-for="item in navItems")
+    //- div.p-4.border.rounded.h-12.flex.items-center
+    //-     ContentList(path="/" v-slot="{ list }")
+    //-         pre list;{{list}}
+    //-     //- pre item;{{ item }}
+    //-     //- template(v-if="item._path")
+    //-         //- NuxtLink(:to="item._path", :prefetch="false") {{ item.title }}
+    //-     //- template(v-else)
+    //-     //-     pre no link
 
 .debug
     //- pre page;{{ page }}
@@ -25,15 +30,25 @@ const resetError = (error: any) => {
 pre.label [category].vue
 .debug.mt-8.flex.space-x-6
     div.w-64.bg-blue-400.prose.shrink-0.p-4
-        ul.list-disc
-            li insert files
-        template(v-for="item in surround")
-            li
-                a(:href="item._path") {{ item.title }}
+        ContentList(:path="$route.params.category" v-slot="{ list }")
+            pre ContentList;
+            div(v-for="article in list.splice(0,5)" :key="article._path")
+                NuxtLink(:to="article._path")
+                    h2 {{ article.title }}
+                    p {{ article.description }}
+        //- ul.list-disc
+        //-     li insert files
+        //- pre {{surround}}
+        //- template(v-for="item in surround")
+        //-     template(v-if="item?._path")
+        //-         li
+        //-             a(:href="item._path || '#'") {{ item.title }}
     div.bg-blue-300.grow.flex.flex-col.space-y-8
         //- h1.text-xl page.title;{{ page.title }}
-        h1.text-xl page.title;{{ page?.title || 'nothing here' }}
+        h1.text-xl.text-red-500 page.title;{{ page?.title || 'nothing here' }}
         //- pre page;{{ page }}
+        //- ContentDoc(value="/business")
+        
         pre.label .src/[category]/[..slug].vue
         pre below:NuxtErrorBoundary > NuxtPage    
         div.prose
--- src/pages/[category]/[...slug].vue
+++ src/pages/[category]/[...slug].vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const { page } = useContent();
+// const { page } = useContent();
 
 const throwError = () => {
   // alert("erro");
@@ -9,12 +9,34 @@ const throwError = () => {
 <template lang="pug">
 main.bg-yellow-400
   pre.label [...slug.vue]
-  h1.text-4xl.font-bold.my-4 category;{{ $route.params.category }}:route;{{ $route.params.slug }}
-  button(@click="throwError",class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded") throwerror
 
   .prose
     //- pre {{ page }}
-    ContentDoc(:key="page.contentId" :value="page")
+    //- The fastest way to query and display your content.
+    //- https://content.nuxtjs.org/api/components/content-doc
+    
+    //- pre {{ doc }}
+    //- The default slot can be used to render the content via v-slot="{ data }" syntax:
+    //- <ContentDoc v-slot="{ doc }">
+    ContentDoc
+      //- template(v-if="doc")
+      //-   p here
+      template(#not-found)
+        h1.text-2xl.font-bold.my-4 not found?
+      template(#empty)
+        h1.text-2xl.font-bold.my-4 Page is empty
+    //- ContentDoc(v-slot="{ doc }")
+    //-   h1.text-4xl.font-bold.my-4 doc.title;{{ doc.title }}
+    //-   template(#not-found)
+    //-     h1 Document not found
+      //- template(v-if="doc")
+      //-   ContentRenderer(:value="doc")
+      
+.debug 
+  h1.text-2xl.font-bold.my-4 category;{{ $route.params.category }}:route;{{ $route.params.slug }}
+  button(@click="throwError",class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded") throwerror
+
+
   //- template(v-if="page")
   //-   pre page;{{ page }}
   //- template(v-else)
